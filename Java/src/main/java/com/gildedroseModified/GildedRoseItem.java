556ab3cbaa44489187b638c4172c44ec6567f45c
HuzaifaTP <91236840+HuzaifaTP@users.noreply.github.com>
2022-08-15 16:57:00 +0800

    removed redundant code & added citation

--- Java/src/main/java/com/gildedroseModified/GildedRoseItem.java
+++ Java/src/main/java/com/gildedroseModified/GildedRoseItem.java
@@ -2,17 +2,23 @@ package com.gildedroseModified;
 
 public interface GildedRoseItem {
 
+String AGED_BRIE = "Aged Brie";
+String LEGENDARY = "Sulfuras, Hand of Ragnaros";
+String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+String CONJURED = "Conjured Mana Cake";
+
+
     static GildedRoseItem gildedRoseItem(Item item) {
-        if (item.name.equals(Item_Match_And_Update.AGED_BRIE)) {
+        if (item.name.equals(AGED_BRIE)) {
             return new AgedBrie(item);
         }
-        if (item.name.equals(Item_Match_And_Update.BACKSTAGE_PASSES)) {
+        if (item.name.equals(BACKSTAGE_PASSES)) {
             return new BackStagePasses(item);
         }
-        if (item.name.equals(Item_Match_And_Update.LEGENDARY)) {
+        if (item.name.equals(LEGENDARY)) {
             return new Legendary();
         }
-        if (item.name.equals(Item_Match_And_Update.CONJURED)) {
+        if (item.name.equals(CONJURED)) {
             return new Conjured(item);
         }
         return new Item_Base(item);
---
+++ Java/src/main/java/com/gildedroseModified/References.md
@@ -0,0 +1,4 @@
+# References/Credits
+
+- Source 1: https://elearning.industriallogic.com/gh/submit?Action=PageAction&album=kata&path=kata/gildedRose/solution&devLanguage=Java
+- Source 2: https://github.com/ivanmoore/gildedrose/tree/possibleSolution
